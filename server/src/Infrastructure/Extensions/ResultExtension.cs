2b60cb2856d96e9c3e9134012427e7418a71e587
DajosPatryk <patryk00.business@gmail.com>
2024-07-20 15:22:50 +0200

    Adjusted ResultExtension to return same format as FluentValidation.

--- server/src/Infrastructure/Extensions/ResultExtension.cs
+++ server/src/Infrastructure/Extensions/ResultExtension.cs
@@ -3,8 +3,10 @@
 public static class ResultExtension
 {
     /// <summary>
-    /// Converts a list of error objects into a standardized format suitable for API error responses. This method 
-    /// constructs an anonymous object that encapsulates the error messages if there are any errors present.
+    /// Converts a list of error objects into a standardized format suitable for API error responses.
+    /// Standard set by Fluent Validation package.
+    /// 
+    /// This method constructs an anonymous object that encapsulates the error messages if there are any errors present.
     /// </summary>
     /// <param name="errors">The list of IError instances, each representing a specific error. Each error should 
     /// include details such as an error message.</param>
